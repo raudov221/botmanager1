2966d2a040e43b399a784f2fb5bb27492cec6177
raudov221 <76117378+raudov221@users.noreply.github.com>
2020-12-20 18:21:59 +0300

    Update ok.py

--- ok.py
+++ ok.py
@@ -3,7 +3,7 @@ bot = Bot('e0273154fd1767633850be5e8758a640188af6147a90eb5b95105050f863707a48d98
 
 @bot.on.message('/mail <text>')
 async def mail(ans: Message, text):
-  m = mailing([id]) # ID бери из настроек приложения
+  m = mailing([api_82735_N1cGUGYqt2f3oPL5YHFoLSSM]) # ID бери из настроек приложения
   m(text)
   await ans('Рассылка началась!')
   a = await m.run_now()
