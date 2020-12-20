1c42d43830dda7046f2f2539545a969ef2c28ef0
raudov221 <76117378+raudov221@users.noreply.github.com>
2020-12-20 18:24:18 +0300

    Update ok.py

--- ok.py
+++ ok.py
@@ -1,9 +1,10 @@
 from vkbottle import Bot, Message, Mailing
 bot = Bot('e0273154fd1767633850be5e8758a640188af6147a90eb5b95105050f863707a48d9837974e3787b2140e', group_id, debug=True)
+mailing = Mailing('api_82735_N1cGUGYqt2f3oPL5YHFoLSSM', bot) # токен бери из настроек приложения 
 
 @bot.on.message('/mail <text>')
 async def mail(ans: Message, text):
-  m = mailing([api_82735_N1cGUGYqt2f3oPL5YHFoLSSM]) # ID бери из настроек приложения
+  m = mailing([id]) # ID бери из настроек приложения
   m(text)
   await ans('Рассылка началась!')
   a = await m.run_now()
