12596243b2c6b55177a48de971539ed94b84e5b8
raudov221 <76117378+raudov221@users.noreply.github.com>
2021-05-11 17:23:31 +0300

    Update ok.py

--- ok.py
+++ ok.py
@@ -9,8 +9,7 @@ user = User("d8ecd8cb345c80a0a2dc573293763baee965fdba2b97e7e3dd107b8d5f786016e3b
 
 @user.on.message_handler(text=".брак пошалить", lower = True)
 async def wrapper(ans: Message):
-	if ans.from_id == "597825377":
-		await ans("@grizzlybot ✅ Принять")
+	await ans("@grizzlybot ✅ Принять")
 
 
 user.run_polling()
